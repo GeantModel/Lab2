2588dcc9f6e31d13a0a57e0059b1908243c97eb4
Yakov <jeykeyel19@gmail.com>
2017-09-18 23:28:04 +0300

    q

--- main.cpp
+++ main.cpp
@@ -1,56 +1,53 @@
 #include <iostream>
 
-void tabl1() {
-    for (int i=1; i<=256; i++){
-        char c = i;
-        if (i%5==0) {
-            printf("%c ", c);
-            std::cout<<"\n";
-        }
-        else printf("%c ", c);
-    }
-}
-
-void tabl2() {
-    for (int i=1; i<=256; i++){
-        char c = i;
-        if (i%2==0)
-            printf("%c ", c);
-         else std::cout<<"\n";
-    }
-}
-void tabl3(int var) {
+int main() {
+    int var;
+    printf("Vvedite nomer varianta 1-5: ");
+    scanf("%d",&var);
+    printf("\n");
     switch (var) {
         case 1:
+            for (int i=1; i<=256; i++){
+                char c = i;
+                if (i%5==0) {
+                    printf("%c ", c);
+                    std::cout<<"\n";
+                }
+                else printf("%c ", c);
+            }
+            break;
+        case 2:
+            for (int i=1; i<=256; i++){
+                char c = i;
+                if (i%2==0)
+                    printf("%c ", c);
+                else std::cout<<"\n";
+            }
+            break;
+        case 3:
             for (int i=5; i<=10; i++){
                 char c = i;
-                 printf("%c ", c); std::cout<<"1";}
-                break;
-        case 2:
+                printf("%c ", c); std::cout<<"1";}
+            break;
+
+        case 4:
             for (int i=20; i=30; i++){
                 char c = i;
                 printf("%c ", c);
                 for (int j=1; j<=11; j++){
-                do printf(" ");
+                    do printf(" ");
                     while (j!=(i%20));
                 }
-                    std::cout<<"\n";}
+                std::cout<<"\n";}
             break;
-        case 3:
+
+        case 5:
             for (int i=50; i>=25; i--){
                 char c = i;
                 printf("%c ", c);
                 std::cout<<"\n";}
-                break;
+            break;
+
         default: printf("Vvedite nomer 1-3");
     }
-}
-int main() {
-   /* tabl1();
-    tabl2();*/
-    int var;
-    printf("Vvedite nomer varianta 1-3: ");
-    scanf("%d",&var);
-    printf("\n");
-    tabl3(var);
 }
